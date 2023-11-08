1fd2baf2773efd28f14c337a5baf4818c8744a02
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2023-11-07 22:51:20 -0600

    Ejercicio1

---
+++ Eje.js
@@ -0,0 +1,18 @@
+//Ejercicio 1 EloquentJavaScript
+
+/*for(let i=0; i<=7; i++){
+    let Contador="*".repeat(i);
+    console.log(Contador);
+}*/
+
+//Ejercicio 2 EloquentJavaScript FizzBuzz/ parte 1: Fizz
+for (let i = 0; i <= 100; i++){
+    
+    if(i%3!=0){
+        console.log(i)
+    }else if(i%3==0){
+        console.log("Fizz");
+    }
+}
+
+//Ejercicio 2 EloquentJavaScript FizzBuzz/ parte 2: Buzz
